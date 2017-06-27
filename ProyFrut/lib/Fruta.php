24e21383f4eda869752b0eab9c41e066dba701b5
Rufino <Rufino@DESKTOP-PLP6QUV>
2017-06-27 00:25:40 -0400

    Agrega clase frutas y primeras funciones

---
+++ ProyFrut/lib/Fruta.php
@@ -0,0 +1,42 @@
+<?php
+
+class Fruta{
+
+var $idfruta;
+var $nomfruta;
+var $descripcion;
+
+       function VerificaFruta(){
+        $oConn=new Conexion();
+        
+        if ($oConn->Conectar())
+            $db=$oConn->objconn;
+        else
+            return false;
+        
+        $sql="SELECT * FROM fruta WHERE nomfruta='$this->nomfruta'";
+        
+        $resultado=$db->query($sql);
+               
+        if ($resultado->num_rows>=1)
+            return true;
+        else
+            return false;
+        
+    }
+    
+        function MostrarFruta(){
+            $oConn=new Conexion();
+            
+            if($oConn->Conectar())
+                $db=$oConn->objconn;
+        else
+            return false;
+        
+        $sql="SELECT FROM fruta WHERE nomfruta='$this->nomfruta";
+        
+        }
+}
+    
+
+  
